ccaa164fc1ae57361eb4879e9ab0202c89cbd0db
gabrieltavares0123 <gabriel.tavaresramos@gmail.com>
2021-06-16 15:24:05 -0300

    (refactor)     -add optimized diveide and conquer algorithm.

--- IntegerPower/IntegerPower/Main.cpp
+++ IntegerPower/IntegerPower/Main.cpp
@@ -4,13 +4,15 @@
 int bfIntegerPower(int b, int n)
 {
     int temp = 1;
-    if (b == 0 && n == 0) {
+    if (b == 0 && n == 0) 
+    {
         std::cout << "Indetermined value!" << std::endl;
         temp = -1;
     }
     else
     {
-        for (int i = n; i > 0; i--) {
+        for (int i = n; i > 0; i--) 
+        {
             temp = temp * b;
         }
     }
@@ -25,15 +27,35 @@ int dcIntegerPower(int b, int n)
     {
         return b;
     }
-    else {
-
+    else 
+    {
         return dcIntegerPower(b, floor((n - 1) / 2)) * dcIntegerPower(b, ceil((n - 1) / 2));
+    }
+}
+
+// Optimized divide and conquer algorithm.
+int odcIntegerPower(int b, int n)
+{
+    if (n == 0)
+    {
+        return b;
+    }
+    else
+    {
+        if (n % 2 == 0)
+        {
+            return odcIntegerPower(b, floor((n - 1) / 2)) * odcIntegerPower(b, floor((n - 1) / 2));
+        }
+        else
+        {
+            return odcIntegerPower(b, floor((n - 1) / 2)) * odcIntegerPower(b, floor((n - 1) / 2)) * b;
+        }
     }
 }
 
 int main()
 {
-    int b = 3;
+    int b = 5;
     int n = 2;
 
     int result = bfIntegerPower(b, n);
@@ -41,4 +63,7 @@ int main()
 
     result = dcIntegerPower(b, n);
     std::cout << "Divide and conquer: " << result << "." << std::endl;
+
+    result = odcIntegerPower(b, n);
+    std::cout << "Optimized divide and conquer: " << result << "." << std::endl;
 }
